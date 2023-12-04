a00d4f64516b40bcd367d5a6b48815f588fe0b09
baliayassin <baliay@tingz.co.il>
2023-12-04 12:16:45 +0200

    react dashboard

---
+++ src/scenes/pie/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import PieChart from "../../components/PieChart";
+
+
+export default function Pie(){
+    return (
+        <Box m={'20px'}>
+             <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+             <Box height={'70vh'}>
+                 <PieChart/>
+             </Box>
+        </Box>
+    )
+}
